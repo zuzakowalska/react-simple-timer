4bf75047f4f8cf8dc1a25ef7f450abcadf8d480c
zuzakowalska <zuzanna@kowalska.pro>
2018-04-22 20:34:59 +0200

    added countdown functionality

--- src/Counter.js
+++ src/Counter.js
@@ -2,36 +2,45 @@ import React, { Component } from 'react';
 import './Counter.css';
 
 class Counter extends Component {
+  
   constructor(props) {
     super(props);
-    this.state = { from: 10, to: 0}
+    this.state = {
+      current: this.props.from
+    }
+    this.handleClick = this.handleClick.bind(this);
   }
 
   countdown() {
-    if (this.state.from > this.state.to) {
-      this.setState((prevState, props) => ({
-      from: prevState.from - 1
-    }));
+    if (this.state.current > this.props.to) {
+      this.setState({
+        current: this.state.current - 1
+      })
+    } else {
+      this.reset();
     }
-    //  else {
-    //   onSuccess();
-    // }
   }
 
-  // onSuccess() {
+  reset(props) {
+    this.setState({
+      current: this.props.from
+    })
+  }
 
-  // }
+  handleClick() {
+    this.reset();
+  }
 
   componentDidMount() {
     setInterval(
       () => this.countdown(),
-    1000)
+      1000)
   }
 
-  render() {
+  render(props) {
     return(
-      <div class="counter">
-        <h1>Counter: { this.state.from }</h1>
+      <div className="counter">
+        <h1 onClick={this.handleClick}>Counter: { this.state.current } </h1>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import Counter from './Counter';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<Counter 
+  from={10} to={0} />, 
+  document.getElementById('root'));
 registerServiceWorker();
